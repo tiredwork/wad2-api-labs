1033cf1b1102c9fc268e8954f662689309ce5f84
Dion Kennedy <20101554@mail.wit.ie>
2025-11-20 12:42:31 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -10,10 +11,11 @@ router.get('/', async (req, res) => {
 });
 
 // create a task
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
+}));
+
 
 // Update Task
 router.put('/:id', async (req, res) => {
@@ -34,7 +36,7 @@ router.delete('/:id', async (req, res) => {
         _id: req.params.id,
     });
     if (result.deletedCount) {
-        res.status(204).json();
+        res.status(204).json({ code:204, msg: 'Task Deleted Successfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
