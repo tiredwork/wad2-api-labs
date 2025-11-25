1c7d08586791eaa5eefca91a62d798353d63384d
Dion Kennedy <20101554@mail.wit.ie>
2025-11-25 11:50:36 +0000

    Salt and Hash

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -8,7 +9,33 @@ const UserSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User'
-    },
+  }
 });
+
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+};
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
+
 
 export default mongoose.model('User', UserSchema);
