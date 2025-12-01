f879114e7ed49f0d98a050921e27bf13fb519220
Dion Kennedy <20101554@mail.wit.ie>
2025-12-01 13:26:18 +0000

    Tasks related to User - lab completed!

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -4,16 +4,19 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
-// Get all tasks
+// Get a user's tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find().populate('userId', 'username');
+    console.log(req.user);
+    const tasks = await Task.find({ userId: `${req.user._id}`});
     res.status(200).json(tasks);
 });
 
 
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
+    const newTask = req.body;
+    newTask.userId = req.user._id;
+    const task = await Task(newTask).save();
     res.status(201).json(task);
 }));
 
@@ -37,11 +40,12 @@ router.delete('/:id', async (req, res) => {
         _id: req.params.id,
     });
     if (result.deletedCount) {
-        res.status(204).json({ code:204, msg: 'Task Deleted Successfully' });
+        res.status(204).json();
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
 });
+    
 
 // Get a user's tasks
 router.get('/user/:uid', async (req, res) => {
