0fe4fc181fcc2de44530bb93ddb24e2f8f207a24
Dion Kennedy <20101554@mail.wit.ie>
2025-12-01 13:06:01 +0000

    Updated profile and start pages

--- tasky-react/src/pages/startPage.jsx
+++ tasky-react/src/pages/startPage.jsx
@@ -1,16 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from '../contexts/authContext';
 import { Link } from "react-router";
 
 const StartPage = () => {
+    const context = useContext(AuthContext);
   
-    return(
-        <>
-            <p>
-                Welcome to Tasky! View your <Link to="/tasks">Tasks</Link> or your <Link to="/profile">Profile</Link>.
-            </p>
-            <p>
-                <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to create tasks!
-            </p>
-        </>
+    return context.isAuthenticated ? (
+        <p>
+            Welcome {context.userName}! View your <Link to="/tasks">Tasks</Link> or your <Link to="/profile">Profile</Link>.
+        </p>
+    ) : (
+        <p>
+            <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to create tasks!
+        </p>
     );
   };
 
